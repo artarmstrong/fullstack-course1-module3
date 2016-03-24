bc76bfeaba195d4079ab0b0f40e5b9281ce5284e
Art Armstrong <art@avelient.com>
2016-03-24 10:54:10 -0700

    Update gitignore and start on solution

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,15 @@
 require 'httparty'
+require 'dotenv'
+Dotenv.load
 
 class Recipe
+    include HTTParty
 
+    base_uri 'http://food2fork.com/api'
+    default_params key: ENV["FOOD2FORK_KEY"]
+    format :json
+
+    def self.for term
+        get("", query: { query: term })["elements"]
+    end
 end
